428460c9c39540f609aabeb8eb68329c4de9cfd3
hangox <liang.hanguang93@gmail.com>
2019-08-11 17:28:20 +0800

    修复编译问题

--- databinding-base/src/androidTest/java/com/hangox/databinding_base/ExampleInstrumentedTest.java
+++ databinding-base/src/androidTest/java/com/hangox/databinding_base/ExampleInstrumentedTest.java
@@ -3,8 +3,8 @@ package com.hangox.databinding_base;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,11 +15,12 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.hangox.databinding_base.test", appContext.getPackageName());
-    }
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("com.hangox.databinding_base.test", appContext.getPackageName());
+  }
 }
