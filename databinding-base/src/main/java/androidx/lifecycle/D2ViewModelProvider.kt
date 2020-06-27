30d71bbbaa2ccb2c234b817458a2d9b62dcd61ea
梁汉光 <lianghanguang@corp.netease.com>
2020-06-27 22:04:01 +0800

    增加不用加入到列表中 ViewModel 工具

---
+++ databinding-base/src/main/java/androidx/lifecycle/D2ViewModelProvider.kt
@@ -0,0 +1,49 @@
+package androidx.lifecycle
+
+import androidx.annotation.MainThread
+import dagger.Reusable
+import javax.inject.Inject
+import javax.inject.Provider
+
+
+private const val KEY: String = "com.netease.cc.mobile.ViewModelProvider.DefaultKey"
+
+/**
+ * 直接注入 ViewModel 用的提供器
+ * T 对应 Owner ,比如 Activity, Fragment 对应的域不一样
+ * V 对应 需要生成的 ViewModel
+ */
+@Reusable
+class D2ViewModelProvider<T : ViewModelStoreOwner, V : ViewModel> @Inject constructor(
+    storeOwner: ViewModelStoreOwner,
+    private val store: ViewModelStore = storeOwner.viewModelStore,
+    private val vModelProvider: Provider<V>
+) {
+
+    val i: V
+
+
+    init {
+        i = get()
+    }
+
+    @MainThread
+    fun <V : ViewModel> get(): V {
+        val canonicalName: String = vModelProvider::class.java.canonicalName
+            ?: throw IllegalArgumentException("Local and anonymous classes can not be ViewModels")
+        return getInside("$KEY:$canonicalName") as V
+    }
+
+    @MainThread
+    private fun <V : ViewModel?> getInside(key: String): V {
+        var viewModel = store.get(key)
+        if (viewModel == null) {
+            viewModel = vModelProvider.get()
+        }
+        // 放到商店中
+        store.put(key, viewModel)
+        return viewModel as V
+    }
+
+
+}
